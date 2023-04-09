9ae761a4b5964044e6e89b768b94832a6ac25491
Imam Hossain <imamhossain98@gmail.com>
2023-04-09 14:41:41 +0600

    first commit

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,17 @@
+"use client";
+
 import React from 'react';
 import Link from 'next/link';
+import {motion} from 'framer-motion';
 
 
-
+const MotionLink = motion(Link);
 
 const Logo = () => {
     return (
         <div className='flex items-center justify-center mt-2'>
-            <Link href='/' className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold'>IM</Link>
+            <MotionLink href='/'
+                className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold'>IM</MotionLink>
         </div>
     );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import Logo from './Logo';
+import Logo from '../components/Logo';
 
 const NavBar = () => {
     return (
