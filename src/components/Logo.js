8426d3ae875984111a258c1b0b08e0e6ffbd15a7
Imam Hossain <imamhossain98@gmail.com>
2023-04-09 18:02:12 +0600

    icon added

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Link from 'next/link';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 
 const MotionLink = motion(Link);
@@ -11,7 +11,11 @@ const Logo = () => {
     return (
         <div className='flex items-center justify-center mt-2'>
             <MotionLink href='/'
-                className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold'>IM</MotionLink>
+                className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold'
+                whileHover={{
+                    background: ["#121212", "rgba(131,58,180,1)", "rgba(253,29,29,1)", "rgba(252,176,69,1)", "rgba(131,58,180,1)", "#121212"],
+                    transition:{duration:1,repeat:Infinity}
+                }}>IM</MotionLink>
         </div>
     );
 };
