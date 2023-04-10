8126e9ab41ecf794d8ca5a9d7c9777fefdc48f15
Imam Hossain <imamhossain98@gmail.com>
2023-04-10 14:24:32 +0600

    home done

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+import Layout from './Layout';
+
+const Footer = () => {
+    return (
+        <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
+            <Layout className='py-8 flex items-center justify-between'>
+                <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+                <div className='flex items-center'>
+                Build by <span className='text-primary text-2xl px-1'>&#9825;</span><Link className='underline underline-offset-2' href="https://www.facebook.com/imam.hossain.7/" target={"_blank"}>Imam Hossain</Link>
+                </div>
+               
+                <Link className='underline underline-offset-2' href="https://wa.me/+8801775984338" target={"_blank"}>Say Hello!</Link>
+            </Layout>
+        </footer>
+    );
+};
+
+export default Footer;
